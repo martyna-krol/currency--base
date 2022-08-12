688d40af14f1e783eea5300bce5a388417d5a191
Martyna Król <martyna.maria.krol@gmail.com>
2022-08-12 18:41:35 +0200

    15.4 4 exercise completed

---
+++ src/components/ResultBox/ResultBox.test.js
@@ -0,0 +1,47 @@
+import ResultBox from './ResultBox';
+import { render , screen , cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { convertPLNToUSD } from './../../utils/convertPLNToUSD';
+import { convertUSDToPLN } from './../../utils/convertUSDToPLN';
+
+  describe('Component ResultBox', () => {
+    it('should render without crashing', () => {
+        render(<ResultBox from="PLN" to="USD" amount={100} />);
+    });
+
+    it('should render proper info about conversion when PLN -> USD', () => {
+
+        const testCases = [
+            { amount: '100' },
+            { amount: '20' },
+            { amount: '200' },
+            { amount: '345' },
+      ];
+
+      for(const testObj of testCases) {
+        render(<ResultBox from='PLN' to='USD' amount={testObj.amount} />);
+        const output = screen.getByTestId('result');
+        const amount = parseInt(testObj.amount)
+        expect(output).toHaveTextContent(`PLN ${amount.toFixed(2)} = ${convertPLNToUSD(amount)}`);
+        cleanup();
+      }
+    })
+
+    it('should render proper info about conversion when USD -> PLN', () => {
+
+        const testCases = [
+            { amount: '100'},
+            { amount: '20'},
+            { amount: '200'},
+            { amount: '345'},
+      ];
+
+      for(const testObj of testCases) {
+        render(<ResultBox from='USD' to='PLN' amount={testObj.amount} />);
+        const output = screen.getByTestId('result');
+        const amount = parseInt(testObj.amount)
+        expect(output).toHaveTextContent(`$${amount.toFixed(2)} = ${convertUSDToPLN(amount)}`);
+        cleanup();
+      }
+    })
+});
